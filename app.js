cf037344c0687eb3e076e59515d429a8aca1fd81
bakakuri <78099139+bakakuri@users.noreply.github.com>
2026-08-20 16:09:40 +0200

    Update Supabase URL and key in app.js

--- app.js
+++ app.js
@@ -34,9 +34,10 @@
 /* ──────────────────────────────────────────────────────────────
    CONFIG  ← შეცვალე შენი Supabase პროექტის მიხედვით
 ──────────────────────────────────────────────────────────────── */
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_KEY = 'YOUR_ANON_PUBLIC_KEY';
+const SUPABASE_URL = 'https://cdtibyfsoqtzodspeabq.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkdGlieWZzb3F0em9kc3BlYWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjg5OTUsImV4cCI6MjEwMjgwNDk5NX0.lagOZBHY7__ZK9XmrYxqeypZSMqhtaxeEZsGyKHuz38';
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
 
 
 /* ──────────────────────────────────────────────────────────────
